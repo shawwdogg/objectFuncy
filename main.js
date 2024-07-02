aee8dbab86702537d58c8af1ce090063ae561a49
Carlos Shaw <cshaw.prsvr@gmail.com>
2024-07-01 20:25:09 -0500

    inital commit

---
+++ main.js
@@ -0,0 +1,56 @@
+//YOUR CODE GOES HERE
+/****************************/
+// Our code here. Don't touch!
+if (typeof getFirstName === 'undefined') {
+    getFirstName = undefined
+  }
+  if (typeof getLastName === 'undefined') {
+    getLastName = undefined
+  }
+  if (typeof getFullName === 'undefined') {
+    getFullName = undefined
+  }
+  if (typeof setFirstName === 'undefined') {
+    setFirstName = undefined
+  }
+  if (typeof setAge === 'undefined') {
+    setAge = undefined
+  }
+  if (typeof giveBirthday === 'undefined') {
+    giveBirthday = undefined
+  }
+  if (typeof marry === 'undefined') {
+    marry = undefined
+  }
+  if (typeof divorce === 'undefined') {
+    divorce = undefined
+  }
+  module.exports = {
+    getFirstName,
+    getLastName,
+    getFullName,
+    setFirstName,
+    setAge,
+    giveBirthday,
+    marry,
+    divorce,
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
